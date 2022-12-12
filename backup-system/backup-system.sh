5a742d38f85b5700fb55baeb654a8a5eefddf4e3
Hunter Dale <guyyatsu@gmail.com>
2022-12-12 04:02:08 -0500

    Adding network graph.

--- backup-system/backup-system.sh
+++
@@ -1,4 +0,0 @@
-SourceDirectory=/server/administrator/backup-system
-BackupDirectory=/server/users/resources/backups
-
-tar -czvf $BackupDirectory/configurations.tar.gz -T $SourceDirectory/configs.txt
